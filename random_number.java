dc5d9f41f19dea09d3848a53aa3c4eda97cad1c9
mighye <bdextermiguel@gmail.com>
2025-10-25 16:39:27 +0800

    Final version

--- random_number.java
+++ random_number.java
@@ -20,8 +20,6 @@ public class random_number {
     int guess;
     int attempts;
     String choice;
-
-
     
 do{
 
@@ -29,7 +27,7 @@ do{
 
     System.out.println("I've picked a secret number between" + BLUE + " 1 " + RESET + "and" + BLUE + " 100." + RESET);
     System.out.println("Try to guess it!\n");
-    
+
     guess = 0;
     attempts = 0;
     secretNumber = random.nextInt(max - min + 1) + 1; // random number between 1 and 100
